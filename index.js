fdaebfac3f1e474048c6449727f8909cadc4efb8
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 20:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts (facts) {
   let i=0;
   while (facts[i]) {
-    facts[i]=facts[i]+"!!!"
+    facts[i]=facts[i].push("!!!")
     i++;
   }
   return facts;
