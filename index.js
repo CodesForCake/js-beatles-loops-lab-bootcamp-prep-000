4be638e2ce7db2cece68bd5bec32437689a89ce4
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 20:54:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function theBeatlesPlay(musicians, instruments) {
   var empty = [];
   for (let i=0; i<musicians.length; i++) {
     if (i===0) {
-      empty.push(musicians[0]+instruments[0]);
+      var first = musicians[0]+instruments[0];
+      empty.push(first);
     }
   }
 }
